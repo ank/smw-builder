40f3368342c94f7c617a3dbc81da9f9b2d9d6c42
Adam Kraut <adamnkraut@gmail.com>
2009-05-08 17:29:48 -0400

    Added SQL adapter

---
+++ sql_adapter.rb
@@ -0,0 +1,29 @@
+require "sequel"
+require "model"
+
+# put this in a yaml file
+DB = Sequel.mysql('454_project', :user => 'root', :password => 'mysqlroot12#', :host => 'localhost')
+
+DB.tables.each do |table|
+  
+  model_name = table.to_s.gsub(/\_/, " ").downcase.capitalize
+  properties = []
+  
+  DB.schema(table).each do |c|
+    property_name = model_name + " " + c[0].to_s.gsub(/\_/," ")
+    property_type = c[1][:type]
+    label = c[0].to_s
+    property_type = :number if property_type == :integer
+    property_type = :string if property_type == nil
+    properties << Property.new(property_name, property_type, label)
+  end
+  
+  # TODO: Do something smart with primary and foreign keys
+  properties.reject!{|p| p.name =~ /id/ }
+  
+  Model.new do |m|
+    m.name = model_name
+    m.properties = properties
+  end
+  
+end
